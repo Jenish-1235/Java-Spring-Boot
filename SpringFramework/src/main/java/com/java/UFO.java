6434baecfdbceafdca8ac5d5794ba25719759076
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-02-20 11:11:09 +0530

    Spring framework - XML Based Beans Configuration

---
+++ SpringFramework/src/main/java/com/java/UFO.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class UFO {
+    String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
