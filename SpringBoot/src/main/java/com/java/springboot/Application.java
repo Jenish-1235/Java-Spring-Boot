b12f73a477c3183a9fa41c8dba5f8dabaf61fd78
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-02-21 19:32:51 +0530

    Spring boot - Intro and Different Layers

--- SpringBoot/src/main/java/com/java/springboot/Application.java
+++ SpringBoot/src/main/java/com/java/springboot/Application.java
@@ -21,3 +21,22 @@ public class Application {
     }
 
 }
+
+
+/*
+    Almost all the attributes we used with spring framework can be used as annotations in spring boot and core purpose of using spring boot is to spend
+    less time on configurations.
+
+
+    Different layers :-
+
+    Client
+
+    Server :- 1. Controller , 2. Service, 3. Repository..
+
+    1. Accepts request and responds to requests.
+    2. Service layer, works for all the processing of data and computations on server.
+    3. Repository collects and dumps data in database and basically acts as data providing layer to service layer and
+    service layer provides the response to controller to propagate back to client.
+
+ */
